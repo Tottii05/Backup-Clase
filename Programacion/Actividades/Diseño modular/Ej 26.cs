895b61cab57c14b51bf4569afc7b7919e51ebce0
Tomas <tomas.vicente.7e5@itb.cat>
2024-01-09 17:24:19 +0100

    agrego cosas

---
+++ Programacion/Actividades/Diseño modular/Ej 26.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        const string Input = "Introduce un valor para poder ver cuantos digitos tiene";
+
+        int num, digits = 1;
+
+        Console.WriteLine(Input);   
+        num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(DigitCalcualtor(num, digits));
+    }
+
+    public static int DigitCalcualtor(int num, int digits)
+    {
+        if (num <10)
+        {
+            return digits;
+        }
+        else
+        {
+            num /=10;
+            return digits += DigitCalcualtor(num, digits);
+        }
+    }
+}
---
+++ Programacion/Actividades/Diseño modular/Ej 27.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        const string Input = "Introduce un valor para poder ver para ver si es primo";
+        const string Prime = "El numero {0} si es primo";
+        const string NoPrime = "El numero {0} no es primo";
+
+        int num, divisor = 2;
+
+        Console.WriteLine(Input);
+        num = Convert.ToInt32(Console.ReadLine());
+        if (PrimeCalculator(num, divisor))
+        {
+            Console.WriteLine(Prime, num);
+        }
+        else
+        {
+            Console.WriteLine(NoPrime, num);
+        }
+    }
+
+    public static bool PrimeCalculator(int num, int divisor)
+    {
+        if (num < divisor)
+        {
+            return false;
+        }
+        if (divisor != num)
+        {
+            if (num % divisor == 0)
+            {
+                return false;
+            }
+            return PrimeCalculator(num, divisor + 1);
+        }
+        return true;
+    }
+}
+
