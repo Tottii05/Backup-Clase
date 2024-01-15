5de446ceeee94a441e20b73eeb44077c84fce4ce
tomas <tomas.vicente.7e5@itb.cat>
2024-01-15 08:00:33 +0100

    recursividad añadido

---
+++ Programacion/Actividades/Pruebas/Potencias.cs
@@ -0,0 +1,29 @@
+﻿namespace MyNameSpace
+{
+    class MyClass
+    {
+        public static void Main()
+        {
+            const string Input = "Introduce la base y el exponente para ver la poténcia";
+
+            int baseNum, powerNum;
+
+            Console.WriteLine(Input);
+            baseNum = Convert.ToInt32(Console.ReadLine());
+            powerNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(RecSum(baseNum, powerNum));
+        }
+
+        public static int RecSum(int baseNum, int powerNum)
+        {
+            if (powerNum == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return baseNum *= RecSum(baseNum,powerNum-1);
+            }
+        }
+    }
+}
---
+++ Programacion/Actividades/Pruebas/SumaFactorial.cs
@@ -0,0 +1,28 @@
+﻿namespace MyNameSpace
+{
+    class MyClass
+    {
+        public static void Main()
+        {
+            const string Input = "Introduce el valor para la suma factorial";
+
+            int num;
+
+            Console.WriteLine(Input);
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(RecSum(num));
+        }
+
+        public static int RecSum(int num)
+        {
+            if (num == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return num += RecSum(num-1);
+            }
+        }
+    }
+}
