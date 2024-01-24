4d466fb010aa0c246306632e981fc7901ea35bb0
Tomas <tomas.vicente.7e5@itb.cat>
2024-01-24 17:27:53 +0100

    ea9 terminado

--- BBDD/UF 3/EA9_VicenteTomas.sql
+++ BBDD/UF 3/EA9_VicenteTomas.sql
@@ -1,4 +1,4 @@
--- EX 1 --
+    -- EX 1 --
 CREATE OR REPLACE FUNCTION trig_nom_departament_notnull() RETURNS TRIGGER LANGUAGE plpgsql AS $$
 BEGIN
     IF NEW.DEPARTMENT_NAME IS NULL THEN
@@ -41,3 +41,63 @@ CREATE TRIGGER trig_restriccions_emp
 INSERT INTO EMPLOYEES VALUES (1, 'juan', 'perez', 'jpez', 5151234566, '1987-07-16', 'AD_PRES', -23, NULL, NULL, 90);
 UPDATE EMPLOYEES SET SALARY = 23000.00 WHERE employee_id = 100;
 UPDATE EMPLOYEES SET SALARY = 25000.00 WHERE employee_id = 100;
+
+-- EX 3 --
+CREATE OR REPLACE FUNCTION trig_comissio() RETURNS TRIGGER LANGUAGE plpgsql AS $$
+    BEGIN
+        IF NEW.COMMISSION_PCT > NEW.SALARY THEN
+            RAISE 'La commisio no pot ser major al salari';
+        END IF;
+        RETURN NEW;
+END;$$;
+
+CREATE TRIGGER trig_comissio
+BEFORE INSERT ON EMPLOYEES
+FOR EACH ROW
+EXECUTE FUNCTION trig_comissio();
+
+INSERT INTO EMPLOYEES VALUES (2, 'sancho', 'perez', 'jpez', 5151234566, '1987-07-16', 'AD_PRES', 0, 0.4, NULL, 90); /*Funciona*/
+INSERT INTO EMPLOYEES VALUES (2, 'sancho', 'panza', 'jpez', 5151234566, '1987-07-16', 'AD_PRES', 0, 1, NULL, 90); /*Salta exepcio*/
+
+-- EX 4 --
+CREATE OR REPLACE FUNCTION trig_errada_modificacio () RETURNS TRIGGER LANGUAGE plpgsql AS $$
+    BEGIN
+        IF NEW.FIRST_NAME <> OLD.FIRST_NAME THEN
+            RAISE EXCEPTION 'No es pot modificar el nom de l empleat';
+        END IF;
+        IF NEW.EMPLOYEE_ID <> OLD.EMPLOYEE_ID THEN
+            RAISE EXCEPTION 'No es pot modificar el codi de l empleat';
+        END IF;
+        IF NEW.SALARY > OLD.SALARY*0.10 THEN
+            RAISE EXCEPTION 'El salari no pot ser superior al 10%% de l anterior';
+        END IF;
+        RETURN NULL;
+END;$$;
+
+CREATE TRIGGER trig_errada_modificacio
+BEFORE UPDATE ON EMPLOYEES
+FOR EACH ROW EXECUTE FUNCTION trig_errada_modificacio();
+
+UPDATE employees SET first_name = 'quijote' WHERE employee_id = 100; /*Salta exepcio del nom*/
+UPDATE employees SET employee_id = 99 WHERE employee_id = 100; /*Salta exepcio del codi */
+UPDATE employees SET salary = 100000 WHERE employee_id = 100; /*Salta exepcio del salari*/
+
+-- EX 5 --
+CREATE TABLE RESAUDITAREMP (
+    RESULTAT VARCHAR(200)
+);
+
+CREATE OR REPLACE FUNCTION trig_auditartaulaemp() RETURNS TRIGGER LANGUAGE plpgsql AS $$
+    BEGIN
+        INSERT INTO RESAUDITAREMP VALUES (CONCAT(TG_NAME, ' - ',TG_WHEN, ' - ',TG_LEVEL, ' - ', TG_OP, ' - ', NOW()));
+        RETURN NULL;
+END;$$;
+
+CREATE TRIGGER trig_auditartaulaemp
+AFTER INSERT OR UPDATE OR DELETE ON EMPLOYEES
+FOR EACH ROW EXECUTE FUNCTION trig_auditartaulaemp();
+
+INSERT INTO EMPLOYEES VALUES (1, 'sancho', 'panza', 'spanz', 5151756566, '1987-06-17', 'AD_PRES', 8300, NULL, NULL, 90);
+UPDATE EMPLOYEES SET MANAGER_ID = 205 WHERE employee_id = 1;
+DELETE FROM EMPLOYEES WHERE employee_id = 1;
+SELECT * FROM RESAUDITAREMP;
