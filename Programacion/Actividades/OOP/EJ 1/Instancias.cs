996839306365b0f0a8caa53251b6e1883a343241
tomas <tomas.vicente.7e5@itb.cat>
2024-01-29 13:59:37 +0100

    objetos

---
+++ Programacion/Actividades/OOP/EJ 1/Instancias.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace prueba
+{
+    public class Person
+    {
+        private string name, last_name;
+        private int age;
+
+        public string Name {  get { return name; } set { name = value; } }
+        public string LastName { get { return last_name; } set { last_name = value; } }
+        public int Age { get { return age; } set { age = value; } }
+    }
+}
---
+++ Programacion/Actividades/OOP/EJ 1/Main.cs
@@ -0,0 +1,19 @@
+﻿using prueba;
+using System;
+using System.IO;
+
+namespace FileHandling
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Person person = new Person();
+            person.Name = "Tomas";
+            person.LastName = "Vicente";
+            person.Age = 18;
+
+            Console.WriteLine(person.Name + " " + person.LastName + " " + person.Age);
+        }
+    }
+}
